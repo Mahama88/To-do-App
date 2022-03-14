8d32350e8bf6c3d07637efa90c038766ca183c38
Mahama88 <sheikibnrauf88@gmail.com>
2022-03-14 13:38:05 +0000

    added some data

--- main.js
+++ main.js
@@ -39,7 +39,7 @@ const setTodo = (arr) => {
   for (el of arr) {
     let html = `<li id=${el}>
             <div class ="todo">
-            <p>$ {el}</p>
+            <p>${el}</p>
             <input type="text" class="hide"/>
             </div>
         
@@ -54,3 +54,48 @@ const setTodo = (arr) => {
   todoList.innerHTML = htmlArr;
   //informText()
 };
+
+//sign in functions
+const signIn = document.querySelector(".logo-wrap p");
+const modal = document.querySelector("#modal");
+const userNm = document.querySelector("input");
+const signBtn = document.querySelector("button");
+const disModal = document.querySelector("span");
+
+signIn.addEventListener("click", () => {
+  modal.style.display = "flex";
+});
+
+disModal.addEventListener("click", (e) => {
+  modal.style.display = "none";
+});
+modal.addEventListener("click", (e) => {
+  e.target == modal ? (modal.style.display = "none") : null;
+});
+
+//function to log in user and save todo list
+signBtn.addEventListener("click", () => {
+  user = userNm.value;
+  if (user == "") {
+    userNm.classList.add("error");
+  } else {
+    userNm.classList.remove("error");
+    signIn.innerText = user;
+    todoArray.clear();
+    setTodo(todoArray);
+    userNm.value = "";
+
+    let db = [...todoArray];
+    let _db = JSON.stringify(db);
+    let userData = localStorage.getItem(user);
+
+    if (userData) {
+      let _userData = JSON.parse(userData);
+      setTodo(todoArray);
+      modal.style.display = "none";
+    } else {
+      localStorage.setItem(user, _db);
+      modal.style.display = "none";
+    }
+  }
+});
